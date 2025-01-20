4b91fbc3539ff2bcd9f1fceede204c182e23cfed
Estiven <estivencamachops@gmail.com>
2025-01-20 16:57:04 +0100

    Meto mi informacion y elimino mensaje predeterminado

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Camacho`s Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Writing about my first things in github pages.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Camacho",
+		email: "estivencamachops@gmail.com",
+		url: "https://camachoo18.github.io/"
 	}
 }
